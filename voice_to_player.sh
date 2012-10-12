b69a7c54c7e36ab2168c7f81e321e46e8614a952
Jin Zhou <ferryzhou@gmail.com>
2012-10-12 19:13:59 -0400

    fixed grammar error.

--- voice_to_player.sh
+++ voice_to_player.sh
@@ -3,8 +3,8 @@ arecord -f S16_LE -d 3 temp.wav;
 
 sox temp.wav temp.flac;
 
-hyp=`curl --data-binary @temp.flac --header "Content-type: audio/x-flac; rate=8000" "https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&pfilter=2&lang=zh-CN&maxresults=6" | grep -Po '"utterance":.*?[^\\]",' | perl -pe 's/"utterance"://; s/^"//; s/",$//'`
+hyp=$(curl --data-binary @temp.flac --header "Content-type: audio/x-flac; rate=8000" "https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&pfilter=2&lang=zh-CN&maxresults=6" | grep -Po '"utterance":.*?[^\\]",' | perl -pe 's/"utterance"://; s/^"//; s/",$//')
 
 echo $hyp
 
-mplayer `ruby run_mp3link.rb $hyp | tail -1`
+mplayer $(ruby run_mp3link.rb $hyp | tail -1)
